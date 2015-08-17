23f5fb6a38d94ce50faeb2b973f6438e49cde5a9
Brian Devins <brian.devins@dealer.com>
2015-08-17 10:19:19 -0400

    Use a symlink

--- karabiner.zsh
+++ karabiner.zsh
@@ -1,6 +1,7 @@
 HERE=${0:a:h}
 
-if [[ `uname` == 'Darwin' ]]
-then
-  cp "$HERE/osx/karabiner/private.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
+if [[ `uname` == 'Darwin' ]] ; then
+  if [[ ! -a "$HOME/Library/Application Support/Karabiner/private.xml" ]] ; then
+    ln -s "$HERE/osx/karabiner/private.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
+  fi
 fi
