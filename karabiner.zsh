8b6327ad7f2238914e643e121cc0911521ea258a
Brian Devins <badevins@gmail.com>
2015-08-17 10:14:26 -0400

    Update karabiner.zsh

--- karabiner.zsh
+++ karabiner.zsh
@@ -1,5 +1,6 @@
 HERE=${0:a:h}
 
-if [ "$(uname)" == "Darwin" ]; then
+if [[ `uname` == 'Darwin' ]]
+then
   cp "$HERE/osx/karabiner/private.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
 fi
