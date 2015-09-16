dbaa0283c805990354664dd5b3ddf3d347e68a8e
Brian Devins <badevins@gmail.com>
2015-09-16 18:58:00 -0400

    Update karabiner.zsh

--- karabiner.zsh
+++ karabiner.zsh
@@ -1,6 +1,7 @@
 HERE=${0:a:h}
 
 if [[ `uname` == 'Darwin' ]] ; then
+  mkdir -p "$HOME/Library/Application Support/Karabiner"
   if [[ ! -a "$HOME/Library/Application Support/Karabiner/private.xml" ]] ; then
     ln -s "$HERE/osx/karabiner/private.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
   fi
