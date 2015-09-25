bb2a47259f967a7bd79ae3d6cdab16bc1c1a381e
Brian Devins <badevins@gmail.com>
2015-09-25 16:04:07 -0400

    Change default shell

--- bootstrap/bootstrap.zsh
+++ bootstrap/bootstrap.zsh
@@ -1,5 +1,7 @@
 #/usr/bin/env zsh
 
+chsh -s /bin/zsh
+
 # Install Antigen
 mkdir -p $HOME/.zshbin
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > $HOME/.zshbin/antigen.zsh
