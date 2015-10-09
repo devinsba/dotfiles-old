61bd7c0b501e12100b85a89c3f30e9051852df15
Brian Devins <badevins@gmail.com>
2015-10-08 20:53:23 -0400

    Create java.zsh

---
+++ java.zsh
@@ -0,0 +1,5 @@
+if  [ -f /usr/libexec/java_home ]; then
+  alias usejava6="export JAVA_HOME=`/usr/libexec/java_home --version 1.6`"
+  alias usejava7="export JAVA_HOME=`/usr/libexec/java_home --version 1.7`"
+  alias usejava8="export JAVA_HOME=`/usr/libexec/java_home --version 1.8`"
+fi
