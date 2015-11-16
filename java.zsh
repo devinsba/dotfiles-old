8047846d270b9b19e5880799ea2806fba9aea493
Brian Devins <badevins@gmail.com>
2015-11-16 18:40:42 -0500

    Update java.zsh

--- java.zsh
+++ java.zsh
@@ -2,3 +2,9 @@ if  [ -f /usr/libexec/java_home ]; then
   alias usejava7="export JAVA_HOME=`/usr/libexec/java_home --version 1.7`"
   alias usejava8="export JAVA_HOME=`/usr/libexec/java_home --version 1.8`"
 fi
+
+if [[ `uname` == 'Linux' ]] ; then
+  if [ -f /etc/debian_version ]; then
+    export JAVA_HOME='readlink -f /usr/bin/java | sed "s:bin/java::"'
+  fi
+fi
