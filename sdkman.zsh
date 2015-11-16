dd359d12af36d51a12a633cb5234a63ecd20bb15
Brian Devins <badevins@gmail.com>
2015-11-16 18:20:43 -0500

    Update sdkman.zsh

--- sdkman.zsh
+++ sdkman.zsh
@@ -1,3 +1,6 @@
 if [[ ! -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] ; then
-  curl -s http://get.sdkman.io | sed -e '/Attempt update of bash profiles.../,$d' | bash
+  (
+    cd $HOME
+    curl -s http://get.sdkman.io | sed -e '/Attempt update of bash profiles.../,$d' | bash
+  )
 fi
