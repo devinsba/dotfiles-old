72e05bf9e450c7c5d74ad0d1ba21963e2fb92b19
Brian Devins <badevins@gmail.com>
2015-11-16 18:30:19 -0500

    Update init.zsh

--- init.zsh
+++ init.zsh
@@ -3,7 +3,7 @@ HERE=${0:a:h}
 source $HERE/java.zsh
 
 for file in `find $HERE -name "*.zsh"`; do
-  if [[ $file =~ ".*(init\.zsh|java\.zsh)$" ]]; then
+  if [[ "$file" =~ ".*(init\.zsh|java\.zsh)$" ]]; then
     echo "$file already loaded"
   else
     source $file
