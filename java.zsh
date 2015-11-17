6e023e7a8ab8ca406edb6a37b72ce7276c53a2f0
Brian Devins <badevins@gmail.com>
2015-11-17 10:48:55 -0500

    Update java.zsh

--- java.zsh
+++ java.zsh
@@ -1,7 +1,7 @@
 if  [ -f /usr/libexec/java_home ]; then
   alias usejava7="export JAVA_HOME=`/usr/libexec/java_home --version 1.7`"
   alias usejava8="export JAVA_HOME=`/usr/libexec/java_home --version 1.8`"
-  usejava7
+  export JAVA_HOME=`/usr/libexec/java_home --version 1.7`
 fi
 
 if [[ `uname` == 'Linux' ]] ; then
