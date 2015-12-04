dd5704a07f991ae0386baa2fc507263d04b6e9ab
Brian Devins <badevins@gmail.com>
2015-12-04 18:26:27 -0500

    Update work.zsh

--- work.zsh
+++ work.zsh
@@ -1,4 +1,4 @@
-if [[ ! -z $NOT_AT_WORK ]] ; then
+if [[ -z "${NOT_AT_WORK+xxx}" ]] ; then
   if [[ -z $WORK_REPO ]] ; then
     echo "Set the work repo with the WORK_REPO envar in .zshrc.local"
   fi
