f8ebc480eea9a44e45a665f1b57e7027158feb5a
Brian Devins <badevins@gmail.com>
2015-12-04 18:13:52 -0500

    Use java 8 by default

--- java.zsh
+++ java.zsh
@@ -1,7 +1,7 @@
 if  [ -f /usr/libexec/java_home ]; then
   alias usejava7="export JAVA_HOME=`/usr/libexec/java_home --version 1.7`"
   alias usejava8="export JAVA_HOME=`/usr/libexec/java_home --version 1.8`"
-  export JAVA_HOME=`/usr/libexec/java_home --version 1.7`
+  export JAVA_HOME=`/usr/libexec/java_home --version 1.8`
 fi
 
 if [[ `uname` == 'Linux' ]] ; then
