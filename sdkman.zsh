b03496c6e9c440165d5a8ac1ea65beacf6c5cd7d
Brian Devins <badevins@gmail.com>
2016-06-29 15:51:23 -0400

    Update sdkman.zsh

--- sdkman.zsh
+++ sdkman.zsh
@@ -4,3 +4,6 @@ if [[ ! -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] ; then
     curl -s http://get.sdkman.io | sed -e '/Attempt update of bash profiles.../,$d' | bash
   )
 fi
+
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
