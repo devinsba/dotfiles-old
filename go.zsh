0275f29c8e3beeb298da07e7bbca239022b286d3
Brian Devins <badevins@gmail.com>
2016-08-21 18:56:06 -0400

    Update go.zsh

--- go.zsh
+++ go.zsh
@@ -1,2 +1,9 @@
+if [[ ! -d "$HOME/.gimme" ]] ; then
+  mkdir -p ~/bin
+  curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+  chmod +x ~/bin/gimme
+fi
+
+eval "$(gimme 1.7)"
 export GOPATH=~/go
 export PATH=$GOPATH/bin:$PATH
