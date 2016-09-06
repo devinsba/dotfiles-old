c2cb29b9aa5a20547ddeb19c166bac6664b1cdb4
Brian Devins <badevins@gmail.com>
2016-09-06 12:24:24 -0400

    Update ruby.zsh

--- ruby.zsh
+++ ruby.zsh
@@ -1,10 +1,10 @@
 [[ ! -s "$HOME/.rvm/scripts/rvm" ]] && curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" && export PATH="$PATH:$HOME/.rvm/bin"
 
-RUBY_VERSION=2.3.1
+MY_RUBY_VERSION=2.3.1
 
-[[ ! -f "$rvm_path/rubies/ruby-$RUBY_VERSION/bin/ruby" ]] && rvm install $RUBY_VERSION
-rvm use --default $RUBY_VERSION
+[[ ! -f "$rvm_path/rubies/ruby-$MY_RUBY_VERSION/bin/ruby" ]] && rvm install $RUBY_VERSION
+rvm use --default $MY_RUBY_VERSION
 
-[[ ! `find $rvm_path/gems/ruby-$RUBY_VERSION/gems/bundler* -type d -quit &> /dev/null` ]] && gem install bundler
-[[ ! -f "$rvm_path/bin/exec_bundle" ]] && rvm wrapper ruby-$RUBY_VERSION exec bundle
+[[ ! `find $rvm_path/gems/ruby-$MY_RUBY_VERSION/gems/bundler* -type d -quit &> /dev/null` ]] && gem install bundler
+[[ ! -f "$rvm_path/bin/exec_bundle" ]] && rvm wrapper ruby-$MY_RUBY_VERSION exec bundle
