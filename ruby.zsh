67372eb112a978c58fd77ece757313035a501dec
Brian Devins <badevins@gmail.com>
2016-09-06 12:22:12 -0400

    Update ruby.zsh

--- ruby.zsh
+++ ruby.zsh
@@ -6,4 +6,5 @@ RUBY_VERSION=2.3.1
 [[ ! -f "$rvm_path/rubies/ruby-$RUBY_VERSION/bin/ruby" ]] && rvm install $RUBY_VERSION
 rvm use --default $RUBY_VERSION
 
-[[ ! `find $rvm_path/gems/ruby-$RUBY_VERSION/gems/bundler* -type d -quit &> /dev/null` ]] && gem install bundler && rvm wrapper ruby-$RUBY_VERSION exec bundle
+[[ ! `find $rvm_path/gems/ruby-$RUBY_VERSION/gems/bundler* -type d -quit &> /dev/null` ]] && gem install bundler
+[[ ! -f "$rvm_path/bin/exec_bundle" ]] && rvm wrapper ruby-$RUBY_VERSION exec bundle
