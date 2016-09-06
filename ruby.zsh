cd88529926115e3ad2397701f2bfbe67b9f187c5
Brian Devins <badevins@gmail.com>
2016-09-06 18:13:22 -0400

    Update for linux

--- ruby.zsh
+++ ruby.zsh
@@ -1,14 +1,23 @@
 [[ ! -s "$HOME/.rvm/scripts/rvm" ]] && curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" && export PATH="$PATH:$HOME/.rvm/bin"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 MY_RUBY_VERSION=2.3.1
 
-[[ ! -f "$rvm_path/rubies/ruby-$MY_RUBY_VERSION/bin/ruby" ]] && rvm install $RUBY_VERSION
-rvm use --default $MY_RUBY_VERSION
+[[ ! -f "$rvm_path/rubies/ruby-$MY_RUBY_VERSION/bin/ruby" ]] && rvm install ruby-$RUBY_VERSION
+rvm use --default ruby-$MY_RUBY_VERSION
 
-if find $rvm_path/gems/ruby-$MY_RUBY_VERSION/gems/bundler* -type d -depth 0 &> /dev/null ; then
+command='find $rvm_path/gems/ruby-$MY_RUBY_VERSION/gems/bundler* -maxdepth 0 -type d &> /dev/null'
+if [[ $(uname) == "Darwin" ]] ; then
+  command='find $rvm_path/gems/ruby-$MY_RUBY_VERSION/gems/bundler* -depth 0 -type d &> /dev/null'
+fi
+
+if $(command) ; then
     echo "gem: bundler already installed"
 else
     gem install bundler
 fi
 [[ ! -f "$rvm_path/bin/exec_bundle" ]] && rvm wrapper ruby-$MY_RUBY_VERSION exec bundle
+
+function __after_rvm() {
+  export PATH="$PATH:$HOME/.rvm/bin"
+}
