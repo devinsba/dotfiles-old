9029e4ca67f9d874a0816f5db13c47756d571038
Brian Devins <badevins@gmail.com>
2016-09-06 12:31:21 -0400

    Update ruby.zsh

--- ruby.zsh
+++ ruby.zsh
@@ -6,5 +6,9 @@ MY_RUBY_VERSION=2.3.1
 [[ ! -f "$rvm_path/rubies/ruby-$MY_RUBY_VERSION/bin/ruby" ]] && rvm install $RUBY_VERSION
 rvm use --default $MY_RUBY_VERSION
 
-[[ ! `find $rvm_path/gems/ruby-$MY_RUBY_VERSION/gems/bundler* -type d -quit &> /dev/null` ]] && gem install bundler
+if find $rvm_path/gems/ruby-$MY_RUBY_VERSION/gems/bundler* -type d -depth 0 &> /dev/null ; then
+    echo "gem: bundler already installed"
+else
+    gem install bundler
+fi
 [[ ! -f "$rvm_path/bin/exec_bundle" ]] && rvm wrapper ruby-$MY_RUBY_VERSION exec bundle
