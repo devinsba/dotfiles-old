d87992a4d42b92820e0c701b75795a702db932f4
Brian Devins <badevins@gmail.com>
2016-09-06 10:35:43 -0400

    install if not included

--- ruby.zsh
+++ ruby.zsh
@@ -1,1 +1,6 @@
+[[ ! -s "$HOME/.rvm/scripts/rvm" ]] && curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" && export PATH="$PATH:$HOME/.rvm/bin"
+
+RUBY_VERSION=2.3.1
+
+[[ ! -f "$rvm_path/rubies/ruby-$RUBY_VERSION/bin/ruby" ]] && rvm install $RUBY_VERSION && rvm use --default $RUBY_VERSION
