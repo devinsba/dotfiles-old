8d4c82447c3694d2d1be9d2daea7876e4c9cdb45
Brian Devins <brian.devins@dealer.com>
2016-10-25 15:33:50 -0400

    Clean up some more

--- lib/clean.zsh
+++ lib/clean.zsh
@@ -7,6 +7,7 @@ function register_cleaner() {
 
 function __zsh_self_destruct() {
     rm -rf $HOME/.antigen
+    rm -rf $HOME/.zcompdump*
     rm -rf $HOME/.zshtools
     rm $HOME/.zshrc
     [ -f $HOME/.zshrc.old ] && mv $HOME/.zshrc.old $HOME/.zshrc
