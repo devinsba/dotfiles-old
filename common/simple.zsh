8dffa57fd08b039f0bfe6ab9e666962ec0b8314a
Brian Devins <badevins@gmail.com>
2016-11-14 13:31:48 -0500

    Update simple.zsh

--- common/simple.zsh
+++ common/simple.zsh
@@ -3,5 +3,5 @@ alias gs="git status"
 
 export DOCKER_HOST=unix:///var/run/docker.sock
 
-export PATH="/usr/local/sbin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 export PATH=$PATH:~/local/bin:~/bin
