4d2389a225c241293bfdb4d8b6a556a85ad674f8
Brian Devins <badevins@gmail.com>
2016-11-14 13:45:34 -0500

    Update karabiner.zsh

--- Darwin/karabiner.zsh
+++ Darwin/karabiner.zsh
@@ -1,7 +1,9 @@
 #@IgnoreInspection BashAddShebang
 HERE=${0:a:h}
 
-mkdir -p "$HOME/Library/Application Support/Karabiner"
-if [[ ! -L "$HOME/Library/Application Support/Karabiner/private.xml" ]] ; then
-    ln -s "$HERE/karabiner/private.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
+if [[ -d "/Applications/Karabiner.app" ]] ; then
+    mkdir -p "$HOME/Library/Application Support/Karabiner"
+    if [[ ! -L "$HOME/Library/Application Support/Karabiner/private.xml" ]] ; then
+        ln -s "$HERE/karabiner/private.xml" "$HOME/Library/Application Support/Karabiner/private.xml"
+    fi
 fi
