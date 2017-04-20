dea5d5b74cbadf6718e54a723b2e40005ba0bf1c
Brian Devins <badevins@gmail.com>
2017-04-20 15:29:38 -0400

    Update terraform.zsh

--- common/terraform.zsh
+++ common/terraform.zsh
@@ -1,3 +1,36 @@
-export TERRAFORM_VERSION=0.8
+function __devinsba_install_tfenv() {
+  (
+    cd $ZSH_LIB_DIR
+    git clone https://github.com/kamatama41/tfenv.git
+    cd $ZSH_BIN_DIR
+    ln -s $ZSH_LIB_DIR/tfenv/bin/* .
+  )
+}
+function __devinsba_install_terraform() {
+  (
+    tfenv install $TERRAFORM_VERSION
+  )
+}
+function __devinsba_update_tfenv() {
+}
+function __devinsba_postinit_tfenv() {
+}
+function __devinsba_cleaner_tfenv() {
+  (
+    cd $ZSH_BIN_DIR
+    rm tfenv terraform
+  )
+}
 
-tfenv use $TERRAFORM_VERSION
+export TERRAFORM_VERSION=0.8.8
+
+if [[ ! -d "$ZSH_LIB_DIR/tfenv" ]] ; then
+  register_installer __devinsba_install_tfenv
+fi
+if [[ ! -d "$ZSH_LIB_DIR/tfenv/versions/$TERRAFORM_VERSION" ]] ; then
+  register_installer __devinsba_install_terraform
+fi
+
+#register_postinit __devinsba_postinit_tfenv
+#register_updater __devinsba_update_tfenv
+#register_cleaner __devinsba_cleaner_tfenv
