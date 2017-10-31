5c245625927ca033d92c3cfdcecbfcd16a04dd31
Brian Devins <badevins@gmail.com>
2017-10-31 13:43:05 -0400

    Add helpers for terraform and terragrunt that bell when done

--- common/tfenv.zsh
+++ common/tfenv.zsh
@@ -1,5 +1,25 @@
 # https://github.com/kamatama41/tfenv
 export PATH="$HOME/.tfenv/bin:$PATH"
 
+function tf() {
+  terraform $@
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+}
+
 # https://github.com/cunymatthieu/tgenv
 export PATH="$HOME/.tgenv/bin:$PATH"
+
+function tg() {
+  terragrunt $@
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+  echo -e "\a"
+}
